90c84e1d9684b4b7a3b8dd3ebd6452e98792e155
valentin noves <novesvalentin@gmail.com>
2021-09-07 19:20:34 -0400

    Added Auth Scope and Model Derivative Formats

---
+++ DynaForge/DynaForge/Authentication/Scope.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication
+{
+    public class Scope
+    {
+        private Scope() { }
+        public static string Set(bool user_profile_read = false, bool user_read = false, bool user_write = false,
+            bool viewables_read = false,bool bucket_create = false, bool bucket_read = false, bool bucket_update = false, bool bucket_delete = false,
+            bool code_all = false, bool account_read = false, bool account_write = false,  bool data_search = false , bool data_create = false , bool data_write = false, 
+            bool data_read = false)
+        {
+            string result = "";
+            result = result + addString(user_profile_read, " user-profile:read");
+            result = result + addString(user_read, " user:read");
+            result = result + addString(user_write, " user:write");
+            result = result + addString(viewables_read, " viewables:read");
+            result = result + addString(data_read, " data:read");
+            result = result + addString(data_write, " data:write");
+            result = result + addString(data_create, " data:create");
+            result = result + addString(data_search, " data:search");
+            result = result + addString(bucket_create, " bucket:create");
+            result = result + addString(bucket_read, " bucket:read");
+            result = result + addString(bucket_update, " bucket:update");
+            result = result + addString(bucket_delete, " bucket:delete");
+            result = result + addString(code_all, " code:all");
+            result = result + addString(account_read, " account:read");
+            result = result + addString(account_write, " account:write");
+
+            return result;
+        }
+
+        private static string addString (bool scopeBool, string value)
+        {
+            string resultString = "";
+            if(scopeBool)
+            {
+                resultString =  value;
+            }
+            return resultString;
+        }
+    }
+}
---
+++ DynaForge/DynaForge/ModelDerivative/Derivatives/GetFormats.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.DesignScript.Runtime;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace ModelDerivative.Derivatives
+{
+    public class GetFormats
+    {
+        private GetFormats() { }
+
+        public static List<string[]> Get(string token)
+        {
+            List<string[]> listformats = new List<string[]>();
+            var client = new RestClient("https://developer.api.autodesk.com/modelderivative/v2/designdata/formats");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", token);
+            request.AddHeader("Cookie", "PF=2PkYhRg1bGqGmTGB9qcxl5");
+            IRestResponse response = client.Execute(request);
+            Rootobject deserializedProduct = JsonConvert.DeserializeObject<Rootobject>(response.Content);
+
+            if (deserializedProduct != null)
+            {
+                listformats.Append(deserializedProduct.formats.dwg);
+                listformats.Append(deserializedProduct.formats.fbx);
+                listformats.Append(deserializedProduct.formats.ifc);
+                listformats.Append(deserializedProduct.formats.iges);
+                listformats.Append(deserializedProduct.formats.obj);
+                listformats.Append(deserializedProduct.formats.step);
+                listformats.Append(deserializedProduct.formats.stl);
+                listformats.Append(deserializedProduct.formats.svf);
+                listformats.Append(deserializedProduct.formats.svf2);
+                listformats.Append(deserializedProduct.formats.thumbnail);
+                return listformats;
+            }
+            else
+            {
+                return listformats;
+            }
+        }
+    }
+
+
+    class Rootobject
+    {
+        public Formats formats { get; set; }
+    }
+
+    class Formats
+    {
+        public string[] dwg { get; set; }
+        public string[] fbx { get; set; }
+        public string[] ifc { get; set; }
+        public string[] iges { get; set; }
+        public string[] obj { get; set; }
+        public string[] step { get; set; }
+        public string[] stl { get; set; }
+        public string[] svf { get; set; }
+        public string[] svf2 { get; set; }
+        public string[] thumbnail { get; set; }
+    }
+
+}
